448168d872da2f8e1195bbcc7f501d5391663d3e
Mike Jackson <perigran_falcon@yahoo.com>
2024-06-24 20:21:36 -0600

    Add files via upload

---
+++ Go_Wiki_CRUD/Go_Wiki_CRUD/cmd/helpers.go
@@ -0,0 +1,30 @@
+/* helpers.go */
+
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"regexp"
+)
+
+var templates = template.Must(template.ParseFiles("./templates/edit.html", "./templates/view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
