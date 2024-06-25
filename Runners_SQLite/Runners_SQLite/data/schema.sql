8ec839e14d64c17b32e350368fbbeea2d080f3a1
Mike Jackson <perigran_falcon@yahoo.com>
2024-06-24 20:27:50 -0600

    Delete Runners_SQLite/Runners_SQLite directory

--- Runners_SQLite/Runners_SQLite/data/schema.sql
+++
@@ -1,9 +0,0 @@
--- runners
-CREATE TABLE IF NOT EXISTS runners (
-    id INTEGER PRIMARY KEY NOT NULL,
-    name VARCHAR(60) NOT NULL,
-    age INTEGER,
-    country VARCHAR(60) NOT NULL,
-    season_best TEXT NOT NULL,
-    personal_best TEXT NOT NULL
-);
