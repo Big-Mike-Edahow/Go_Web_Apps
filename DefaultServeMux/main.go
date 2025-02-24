5c1d1f0641bdb0acdde0db4a37a0dffe5f1a5158
Mike Jackson <perigran_falcon@yahoo.com>
2025-02-23 17:49:31 -0700

    Create main.go

---
+++ DefaultServeMux/main.go
@@ -0,0 +1,37 @@
+/* main.go */
+
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	log.Println("Listening and serving on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", logRequest(http.DefaultServeMux)))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	indexTemplate := template.Must(template.ParseFiles("./templates/index.html"))
+	indexTemplate.Execute(w, nil)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	aboutTemplate, _ := template.ParseFiles("./templates/about.html")
+	aboutTemplate.Execute(w, nil)
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s %s\n", r.RemoteAddr, r.Proto, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
